c0d093c5881ea1c74f16730a45e681d0309e7d5f
eric valcik <eric.valcik@gmail.com>
2023-07-20 00:41:55 +0200

    add load animation

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,50 +1,55 @@
 "use client";
 
-import React, { FC, useEffect } from "react";
+import React, { FC, useEffect, useState } from "react";
 import Image from "next/image";
 import loadTop from "../../public/load-top.png";
 import loadBottom from "../../public/load-bottom.png";
+import { motion } from "framer-motion";
 
 export const LoadingScreen: FC = () => {
-  const [open, setOpen] = React.useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setOpen(open + 1);
-    }, 100);
-    return () => clearInterval(interval);
-  }, [setOpen, open]);
-
   return (
-    <>
-      <div
-        className="absolute inset-0 w-screen h-screen z-30 overflow-hidden"
-        style={{ top: `-${open}px` }}
-      >
-        <Image
-          src={loadTop}
-          alt="load-top"
-          sizes="100vh"
-          style={{
-            height: "100%",
-            objectFit: "cover",
+    <div className="relative">
+      <div className="overflow-hidden">
+        <motion.div
+          className="absolute inset-0 w-screen h-screen z-30 overflow-hidden"
+          animate={{
+            y: -window.innerHeight / 2,
+            opacity: 0,
+            transitionEnd: { display: "none" },
+            transition: { duration: 0.7, delay: 3, ease: "easeIn" },
           }}
-        />
-      </div>
-      <div
-        className="absolute inset-0 w-screen h-screen z-30 overflow-hidden"
-        style={{ top: `${open}px` }}
-      >
-        <Image
-          src={loadBottom}
-          alt="load-bottom"
-          sizes="100vh"
-          style={{
-            height: "100%",
-            objectFit: "cover",
+        >
+          <Image
+            src={loadTop}
+            alt="load-top"
+            sizes="100vh"
+            style={{
+              height: "100%",
+              objectFit: "cover",
+            }}
+          />
+        </motion.div>
+        <motion.div
+          className="absolute inset-0 w-screen h-screen z-30 overflow-hidden"
+          animate={{
+            y: window.innerHeight / 2,
+            opacity: 0,
+            transitionEnd: { display: "none" },
+            transition: { duration: 0.7, delay: 3, ease: "easeIn" },
           }}
-        />
+        >
+          <Image
+            src={loadBottom}
+            alt="load-bottom"
+            sizes="100vh"
+            style={{
+              height: "100%",
+              objectFit: "cover",
+              overflow: "hidden",
+            }}
+          />
+        </motion.div>
       </div>
-    </>
+    </div>
   );
 };

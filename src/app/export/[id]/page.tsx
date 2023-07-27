cf3398222febacdb8135ba119081cda245cdd89a
eric valcik <eric.valcik@gmail.com>
2023-07-27 11:13:52 +0200

    make final uzi downloadable

--- src/app/export/[id]/page.tsx
+++ src/app/export/[id]/page.tsx
@@ -2,6 +2,10 @@
 
 import Image from "next/image";
 import { cn } from "@/utils/cn";
+import logo from "../../../../public/loading/logo.png";
+import Link from "next/link";
+import { joystix } from "@/utils/font";
+import React from "react";
 
 export default async function ExportPage({
   params: { id },
@@ -12,21 +16,37 @@ export default async function ExportPage({
   const fileName = id;
 
   return (
-    <div className="flex items-center justify-center mx-auto w-[390px]">
-      <div
-        className={cn(
-          "flex flex-col items-center justify-center h-screen w-[390px] py-24 px-4",
-        )}
-      >
-        <Image
-          src={`/exports/${id}.png`}
-          alt="export-img"
-          className="border-4 border-solid border-[#ff1aec] drop-shadow-[0_0_0.35rem_#ff1aec]"
-          height={478}
-          width={376}
-        />
+    <main className="fixed w-screen flex h-screen flex-col items-center justify-between md:p-24 py-4">
+      <div className="flex flex-col items-center justify-center h-full w-[390px]">
+        <div className={cn("flex flex-col items-center h-screen")}>
+          <Image
+            src={logo}
+            alt="uzi-logo"
+            className="select-none mb-10"
+            height={100}
+            width={200}
+          />
+          <Image
+            src={`/exports/${id}.png`}
+            alt="export-img"
+            className="border-4 border-solid border-[#ff1aec] drop-shadow-[0_0_0.35rem_#ff1aec] mb-10"
+            height={432}
+            width={340}
+          />
+
+          <a href={`/exports/${id}.png`} download={true}>
+            <button
+              className={cn(
+                "drop-shadow-[0_0_0.35rem_#ff1aec] hover:drop-shadow-[0_0_0.35rem_white] select-none flex items-center justify-center gap-4 text-[#ff1aec] border-2 border-[#ff1aec] px-4 py-2 transition-all duration-200 group hover:border-white hover:text-white",
+                joystix.className,
+              )}
+            >
+              DOWNLOAD YOUR UZI
+            </button>
+          </a>
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
 

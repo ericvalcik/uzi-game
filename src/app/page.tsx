365d67ced76c88cc9ddf1b14b07ecff8a2e974ca
eric valcik <eric.valcik@gmail.com>
2023-07-27 15:14:36 +0200

    create placeholder page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,21 @@
 import React, { useState } from "react";
 import Game from "@/components/Game";
 import { LoadingScreen } from "@/components/LoadingScreen";
+import { joystix } from "@/utils/font";
+import { cn } from "@/utils/cn";
 
 export default function Home() {
-  const [renderPage, setRenderPage] = useState(false);
-
-  if (renderPage) {
-    return <Game setRenderPage={setRenderPage} />;
-  }
-
-  return <LoadingScreen setRenderPage={setRenderPage} />;
+  return (
+    <div
+      className={cn(
+        "w-screen h-screen flex flex-col items-center justify-center text-sm p-6",
+        joystix.className,
+      )}
+      style={{
+        filter: `drop-shadow(0 0 0.4rem white)`,
+      }}
+    >
+      Come back later, maybe we will be launched by then
+    </div>
+  );
 }

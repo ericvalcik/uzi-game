62f94ef724eb2d0d05a1515f8be980f9bd8c7983
eric valcik <eric.valcik@gmail.com>
2023-07-27 12:07:23 +0200

    add share button and share image

--- src/app/export/[id]/page.tsx
+++ src/app/export/[id]/page.tsx
@@ -6,9 +6,8 @@ import logo from "../../../../public/loading/logo.png";
 import Link from "next/link";
 import { joystix } from "@/utils/font";
 import React from "react";
-import dynamic from "next/dynamic";
 
-export default async function ExportPage({
+export default function ExportPage({
   params: { id },
 }: {
   params: { id: string };
@@ -17,7 +16,7 @@ export default async function ExportPage({
   const fileName = id;
 
   return (
-    <main className="fixed w-screen flex h-screen flex-col items-center justify-between md:p-24 py-4">
+    <main className="w-screen flex min-h-screen flex-col items-center justify-between md:p-24 py-4">
       <div className="flex flex-col items-center justify-center h-full w-[390px]">
         <div className={cn("flex flex-col items-center h-screen")}>
           <Link href="/">
@@ -37,25 +36,29 @@ export default async function ExportPage({
             width={340}
           />
           <button
-            onClick={() => {
-              const image = new File([`/exports/${id}.png`], fileName, {
-                type: "image/png",
-              });
-              if (
-                navigator.canShare &&
-                navigator.canShare({ files: [image] })
-              ) {
-                navigator
-                  .share({
-                    files: [image],
-                    title: "Uzi Export",
-                    text: "The dopest uzi fosho.",
-                  })
-                  .then(() => console.log("Share was successful."))
-                  .catch((error) => console.log("Sharing failed", error));
-              } else {
-                console.log(`Your system doesn't support sharing files.`);
-              }
+            onClick={async () => {
+              fetch(`/exports/${id}.png`)
+                .then((res) => res.blob())
+                .then((blob) => {
+                  const image = new File([blob], `${fileName}.png`, {
+                    type: "image/png",
+                  });
+                  if (
+                    navigator.canShare &&
+                    navigator.canShare({ files: [image] })
+                  ) {
+                    navigator
+                      .share({
+                        files: [image],
+                        title: "Uzi Export",
+                        text: "The dopest uzi fosho.",
+                      })
+                      .then(() => console.log("Share was successful."))
+                      .catch((error) => console.log("Sharing failed", error));
+                  } else {
+                    console.log(`Your system doesn't support sharing files.`);
+                  }
+                });
             }}
             className={cn(
               "mb-6 drop-shadow-[0_0_0.35rem_#ff1aec] hover:drop-shadow-[0_0_0.35rem_white] select-none flex items-center justify-center gap-4 text-[#ff1aec] border-2 border-[#ff1aec] px-4 py-2 transition-all duration-200 group hover:border-white hover:text-white",
